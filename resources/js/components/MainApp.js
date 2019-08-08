59260a81dbd4c0d19a3ff48d64dc941c70713c01
Simeon Dominiq <simeondominiq@gmail.com>
2019-08-08 17:53:43 +0200

    Debugging

--- resources/js/components/MainApp.js
+++ resources/js/components/MainApp.js
@@ -1,4 +1,4 @@
-import { Component, Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import LoginPage from "../components/LoginPage/LoginPage";
 
 export default class MainApp extends Component {

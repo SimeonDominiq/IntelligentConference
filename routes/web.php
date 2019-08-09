2a8920d56ca0e1a24730ace8a00a9ab7bd5fee61
Simeon Dominiq <simeondominiq@gmail.com>
2019-08-09 11:09:43 +0200

    Modified web php file

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function() {
-    dd("HomePage");
-});
+// Route::get('/', function() {
+//     dd("HomePage");
+// });
 Route::get('/hello', function() {
     dd("Hello");
 });
